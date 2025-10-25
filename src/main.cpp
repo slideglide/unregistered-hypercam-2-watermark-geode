25faa2119d0e77e99cdb3dd9f4b81e642c671175
slideglide <68702247+slideglide@users.noreply.github.com>
2025-10-26 02:48:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@
 using namespace geode::prelude;
 
 class $modify(HyperCamWatermarkPLHook, PlayLayer) {
-  void setupHasCompleted() { 
-    auto sprite = CCSprite::create("unregistered.png"_spr);
-    if (sprite) {
-      auto winSize = CCDirector::sharedDirector()->getWinSize();
-      sprite->setAnchorPoint(CCPoint(0, 1));
-      sprite->setScale(0.325f);
-      sprite->setPosition(CCPoint(0, winSize.height));
-      PlayLayer::setupHasCompleted();
-      this->addChild(sprite);
-      sprite->setID("hypercam-watermark"_spr);
+    void setupHasCompleted() {
+        PlayLayer::setupHasCompleted();
+        auto sprite = CCSprite::create("unregistered.png"_spr);
+        if (!sprite) return;
+        auto safeArea = geode::utils::getSafeAreaRect();
+        sprite->setAnchorPoint({0, 1});
+        sprite->setScale(0.325f);
+        sprite->setPosition({
+            safeArea.origin.x,
+            safeArea.origin.y + safeArea.size.height
+        });
+        this->addChild(sprite);
+        sprite->setID("hypercam-watermark"_spr);
     }
-  }
 };
