49d820936722258c9590b45e7ab120fcdfdf6495
slideglide <68702247+slideglide@users.noreply.github.com>
2025-10-26 01:37:01 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Unregistered HyperCam 2
+
+Add the funny watermark from the unregistered version of the HyperCam 2 screen recorder to your gameplay!
+
+**HyperCam is a trademark of Hyperionics Technology LLC. This mod is not affiliated with or endorsed by Hyperionics Technology LLC.**
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(HyperCamWatermarkPLHook, PlayLayer) {
+  void setupHasCompleted() { 
+    auto sprite = CCSprite::create("unregistered.png"_spr);
+    if (sprite) {
+      auto winSize = CCDirector::sharedDirector()->getWinSize();
+      sprite->setAnchorPoint(CCPoint(0, 1));
+      sprite->setScale(0.325f);
+      sprite->setPosition(CCPoint(0, winSize.height));
+      PlayLayer::setupHasCompleted();
+      this->addChild(sprite); 
+    }
+  }
+};
